36580322d572162ad7feaf7c748ebbc8a31fee62
fangdai <65178777+fangdai-dear@users.noreply.github.com>
2023-08-16 14:56:02 +0800

    Update result_image.py

--- result_image.py
+++ result_image.py
@@ -66,29 +66,3 @@ base_auc1 = [0.875,0.668,0.855,0.727,0.771,0.837,0.734,0.740,0.697,0.873,0.880,0
 title1 = 'CXP datasets subgroup: Female 18-40, 40-60, 60-80, >80)'
 save1 = "/export/home/daifang/CXP/figure/test_age(female).png"
 create_multi_bars(label, data1, base_auc1, AUC1, color1, text_label1, title1, save1, bar_gap=0.03)
-
-
-# data2 = []
-# df11 = pd.read_csv('/export/home/daifang/CXP/result/Female_rate/Female_rate_auc.txt', sep=',', names=label)
-# for indexs in df11.index:
-#         rowData = df11.loc[indexs].values[0:14]
-#         rowData = rowData.tolist()
-#         data2.append(rowData)
-# AUC2 = []
-# auc22 = pd.read_csv('/export/home/daifang/CXP/result/Female_rate/Female_rate_after.txt', sep=',', names=label)
-# for indexs in auc22.index:
-#         rowData = auc22.loc[indexs].values[0:14]
-#         rowData = rowData.tolist()
-#         AUC2.append(rowData)
-
-
-# color2 = ['gold','dimgray','deepskyblue','orange']
-# text_label2 = ['Female:Asian (2398)','Female:Black (973)','Female:White (9499)','Female:Other (3692)']
-
-# base_auc2 = [0.875,0.698,0.851,0.747,0.76,0.829,0.754,0.777,0.727,0.873,0.860,0.785,0.760,0.868]
-
-# title2 = 'CXP datasets subgroup: Female: Asian, Black, White, Other' 
-
-# save2 = "/export/home/daifang/CXP/figure/test_rate(female).png"
-
-# create_multi_bars(label, data2, base_auc2, AUC2, color2, text_label2, title2, save2,bar_gap=0.03)
