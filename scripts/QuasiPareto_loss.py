847c0fb79317c311d7cc4dd63d1337a0eb44a417
fangdai <65178777+fangdai-dear@users.noreply.github.com>
2023-08-16 14:54:03 +0800

    Update QuasiPareto_loss.py

--- scripts/QuasiPareto_loss.py
+++ scripts/QuasiPareto_loss.py
@@ -5,24 +5,36 @@ import torch.nn.functional as F
 import numpy as np
 
 def Divide(outputs, labels, subggroup):
-    Min_img, Min_lab, Maj_img, Maj_lab = torch.ones(0, len(labels[0])), torch.ones(0, len(labels[0])), \
-                                        torch.ones(0, len(labels[0])), torch.ones(0, len(labels[0]))
-    for x in range(len(subggroup)):
-        if subggroup[x].tolist()[0] == 1 :
-            Min_img = torch.cat((Min_img, outputs[x].detach().unsqueeze(0)), 0)
-            Min_lab = torch.cat((Min_lab, labels[x].detach().unsqueeze(0)), 0)
-        else:
-            Maj_img = torch.cat((Maj_img, outputs[x].detach().unsqueeze(0)), 0)
-            Maj_lab = torch.cat((Maj_lab, labels[x].detach().unsqueeze(0)), 0)
-    return Maj_img, Maj_lab, Min_img, Min_lab
+    def del_tensor_ele(arr,index):
+        arr1 = arr[0:index]
+        arr2 = arr[index+1:]
+        return torch.cat((arr1,arr2), dim=0)
+    Min_img, Maj_img, sub_maj, Min_lab, Maj_lab, sub_min  = outputs, outputs, subggroup, labels, labels, subggroup
+    for j in range(len(subggroup)):
+        for x in range(len(sub_maj)):
+            if sub_maj[x].tolist()[0] == 0:
+                Maj_img = del_tensor_ele(Maj_img,x)
+                Maj_lab = del_tensor_ele(Maj_lab,x)
+                sub_maj = del_tensor_ele(sub_maj,x)
+                break
+    for j in range(len(subggroup)):
+        for x in range(len(sub_min)):
+            if sub_min[x].tolist()[0] == 1:
+                Min_img = del_tensor_ele(Min_img,x)
+                Min_lab = del_tensor_ele(Min_lab,x)
+                sub_min = del_tensor_ele(sub_min,x)
+                break
+
+    return Maj_img, Maj_lab, sub_maj, Min_img, Min_lab, sub_min
+
 
 class MMDLoss(nn.Module):
     def __init__(self, kernel_type='rbf', kernel_mul=2.0, kernel_num=5, fix_sigma=None, **kwargs):
         super(MMDLoss, self).__init__()
-        self.kernel_num = kernel_num
-        self.kernel_mul = kernel_mul
-        self.fix_sigma = None
-        self.kernel_type = kernel_type
+        kernel_num = kernel_num
+        kernel_mul = kernel_mul
+        fix_sigma = None
+        kernel_type = kernel_type
 
     def guassian_kernel(self, source, target, kernel_mul, kernel_num, fix_sigma):
         n_samples = int(source.size()[0]) + int(target.size()[0])
@@ -43,46 +55,49 @@ class MMDLoss(nn.Module):
                       for bandwidth_temp in bandwidth_list]
         return sum(kernel_val)
 
-    def linear_mmd2(self, f_of_X, f_of_Y):
-        loss = 0.0
-        delta = f_of_X.float().mean(0) - f_of_Y.float().mean(0)
-        loss = delta.dot(delta.T)
-        return loss
+    
 
     def forward(self, subggroup, outputs):
-        Min_img, Maj_img, = torch.ones(0, 100), torch.ones(0, 100)
-        for x in range(len(subggroup)):
-            if subggroup[x].tolist()[0] == 1 :
-                Min_img = torch.cat((Min_img, outputs[x].detach().unsqueeze(0)), 0)
-            else:
-                Maj_img = torch.cat((Maj_img, outputs[x].detach().unsqueeze(0)), 0)
+        Min_img,sub_maj, Maj_img, sub_min = outputs, subggroup, outputs, subggroup
+        def del_tensor_ele(arr,index):
+                arr1 = arr[0:index]
+                arr2 = arr[index+1:]
+                return torch.cat((arr1,arr2), dim=0)
+        for j in range(len(subggroup)):
+            for x in range(len(sub_maj)):
+                if sub_maj[x].tolist()[0] == 0:
+                    Maj_img = del_tensor_ele(Maj_img,x)
+                    sub_maj = del_tensor_ele(sub_maj,x)
+                    break
+        for j in range(len(subggroup)):
+            for x in range(len(sub_min)):
+                if sub_min[x].tolist()[0] == 1:
+                    Min_img = del_tensor_ele(Min_img,x)
+                    sub_min = del_tensor_ele(sub_min,x)
+                    break
 
         source = Min_img
         target = Maj_img
-
-        if self.kernel_type == 'linear':
-            return self.linear_mmd2(source, target)
-        elif self.kernel_type == 'rbf':
-            batch_size = int(source.size()[0])
-            kernels = self.guassian_kernel(
-                source, target, kernel_mul=self.kernel_mul, kernel_num=self.kernel_num, fix_sigma=self.fix_sigma)
-            XX = torch.mean(kernels[:batch_size, :batch_size])
-            YY = torch.mean(kernels[batch_size:, batch_size:])
-            XY = torch.mean(kernels[:batch_size, batch_size:])
-            YX = torch.mean(kernels[batch_size:, :batch_size])
-            loss = torch.mean(XX + YY - XY - YX)
-            return loss
+        batch_size = int(source.size()[0])
+        kernels = guassian_kernel(
+            source, target, kernel_mul=kernel_mul, kernel_num=kernel_num, fix_sigma=fix_sigma)
+        XX = torch.mean(kernels[:batch_size, :batch_size])
+        YY = torch.mean(kernels[batch_size:, batch_size:])
+        XY = torch.mean(kernels[:batch_size, batch_size:])
+        YX = torch.mean(kernels[batch_size:, :batch_size])
+        loss = torch.mean(XX + YY - XY - YX)
+        return loss
 
 class BatchSpectralShrinkage(nn.Module):
 
     def __init__(self, k=1):
         super(BatchSpectralShrinkage, self).__init__()
-        self.k = k
+        k = k
 
     def forward(self, feature):
         result = 0
         u, s, v = torch.svd(feature.t())
         num = s.size(0)
-        for i in range(self.k):
+        for i in range(k):
             result += torch.pow(s[num-1-i], 2)
         return result
